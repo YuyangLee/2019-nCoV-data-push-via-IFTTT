f9f3a159d9d2ffe68b8b417a7d735ffa81e239bc
AP0STR0PH3 <yuyanglee2002@outlook.com>
2020-02-01 00:03:21 +0800

    Create IFTTT_push_end.py
    finished full service

---
+++ IFTTT_push_end.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import requests
+from datetime import datetime
+
+post_url = "https://maker.ifttt.com/trigger/program_push/with/key/fP7Zt7dO7IqmDBZI49uEUfagP4rnYK9gD5jTLYmKMRG"
+
+push_val1_text = "2019-nCoV 数据推送\\n推送时间：" + datetime.now().strftime("%Y-%m-%d %H:%M") + " GMT+8\\n推送设备：" + host_name + "\\n推送内容："
+push_val2_text = "\\n" + "程序下线。"
+push_val3_text = "\\n" + "如果没有提前接到通知，请拨冗通知服务运营者，谢谢！" + "\\n"
+body = "{ \"value1\": \"" + push_val1_text + "\", \"value2\": \"" + push_val2_text + "\", \"value3\": \"" + push_val3_text + "\" }"
+headers = { 'Content-Type': 'application/json' }
+
+requests.post(post_url, data = body.encode('utf-8'), headers = headers)
