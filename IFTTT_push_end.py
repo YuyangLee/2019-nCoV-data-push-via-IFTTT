6060ebfaf6c809711a09592e1884c8e85ac7c76a
AP0STR0PH3 <yuyanglee2002@outlook.com>
2020-02-01 00:04:12 +0800

    Update IFTTT_push_end.py

--- IFTTT_push_end.py
+++ IFTTT_push_end.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import requests
+import socket
 from datetime import datetime
 
 post_url = "https://maker.ifttt.com/trigger/program_push/with/key/fP7Zt7dO7IqmDBZI49uEUfagP4rnYK9gD5jTLYmKMRG"
+host_name = socket.gethostname()
 
 push_val1_text = "2019-nCoV 数据推送\\n推送时间：" + datetime.now().strftime("%Y-%m-%d %H:%M") + " GMT+8\\n推送设备：" + host_name + "\\n推送内容："
 push_val2_text = "\\n" + "程序下线。"
