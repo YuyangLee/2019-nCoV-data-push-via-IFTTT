d4cfd32d8b1ca48ecf44ee9335245c8153382a2d
AP0STR0PH3 <yuyanglee2002@outlook.com>
2020-02-01 01:04:23 +0800

    Create IFTTT_push_end.py

---
+++ IFTTT_push_end.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import requests
+import socket
+import pandas as paths
+from datetime import datetime
+
+get_Data_URL = "https://view.inews.qq.com/g2/getOnsInfo"
+post_url_p1 = "https://maker.ifttt.com/trigger/"
+post_url_p2 = "/with/key/"
+post_urls = ["https://view.inews.qq.com/g2/getOnsInfo/program_push/with/key/fP7Zt7dO7IqmDBZI49uEUfagP4rnYK9gD5jTLYmKMRG",]
+
+def read_urls(path):
+    a = paths.read_csv(path)
+    names = a['event_name']
+    keys  = a['key']
+    urls  = [post_url_p1 + names[i] + post_url_p2 + keys[i] for i in range(len(names))]
+    return urls
+
+host_name = socket.gethostname()
+
+push_val1_text = "2019-nCoV 数据推送\\n推送时间：" + datetime.now().strftime("%Y-%m-%d %H:%M") + " GMT+8\\n推送设备：" + host_name + "\\n推送内容："
+push_val2_text = "\\n" + "程序下线。"
+push_val3_text = "\\n" + "如果没有提前接到通知，请拨冗通知服务运营者，谢谢！" + "\\n\\n"
+body = "{ \"value1\": \"" + push_val1_text + "\", \"value2\": \"" + push_val2_text + "\", \"value3\": \"" + push_val3_text + "\" }"
+headers = { 'Content-Type': 'application/json' }
+
+for url in post_urls:
+    requests.post(url, data = body.encode('utf-8'), headers = headers)
