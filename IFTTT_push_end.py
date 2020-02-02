148841338a38c1c366f93b1ab6745a69eae21908
Ap0str0ph3 <yuyanglee2002@outlook.com>
2020-02-02 15:55:45 +0800

    Update IFTTT_push_end.py

--- IFTTT_push_end.py
+++ IFTTT_push_end.py
@@ -9,7 +9,6 @@ sub_doc_path = 'subscribed_urls.csv'
 
 post_url_p1 = "https://maker.ifttt.com/trigger/"
 post_url_p2 = "/with/key/"
-post_urls = ["https://maker.ifttt.com/trigger/program_push/with/key/fP7Zt7dO7IqmDBZI49uEUfagP4rnYK9gD5jTLYmKMRG",]
 
 def read_urls(path):
     a = paths.read_csv(path)
